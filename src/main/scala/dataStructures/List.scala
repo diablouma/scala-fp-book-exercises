2e9e7af805bd0017018dd8469bcb1d0f091699e8
diablouma <mh732@hotmail.com>
2020-11-14 17:59:07 +0100

    comments about variance annotation

--- src/main/scala/dataStructures/List.scala
+++ src/main/scala/dataStructures/List.scala
@@ -1,8 +1,10 @@
 package dataStructures
 
-sealed trait List[+A]
-case object Nil extends List[Nothing]
-case class Cons[+A](head: A, tail: List[A]) extends List[A]
+// sealed: all the implementations must be in this file
+sealed trait List[+A] // + covariant means that if A is subtype of B, then List[A] is subtype of List[B]
+case object Nil extends List[Nothing] // first data constructor, declared with case// Nothing is a subtype of all types
+// then List[Nothing] is a subtype of all List so Nil will be subtype of any List
+case class Cons[+A](head: A, tail: List[A]) extends List[A] // second data constructor
 
 object List {
   def sum(ints: List[Int]): Int = ints match {
