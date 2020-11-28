854e731e73f0a2a06aeb77d66850abfa0bbd4450
diablouma <mh732@hotmail.com>
2020-11-28 18:48:13 +0100

    exercise 3_23

--- src/main/scala/dataStructures/List.scala
+++ src/main/scala/dataStructures/List.scala
@@ -49,6 +49,14 @@ object List {
     }
   }
 
+  def zipWith[A, B, C](a: List[A], b: List[B])(f: (A, B) => C): List[C] = {
+    (a,b) match {
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (Cons(h1,t1), Cons(h2,t2)) => Cons(f(h1, h2), zipWith(t1,t2)(f))
+    }
+  }
+
   def filterUsingFlatMap[A](l: List[A])(f: A=> Boolean): List[A] = {
     flatMap(l)((item) => {
       if(f(item)) {
