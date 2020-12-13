7e9db28becbbb6fe962f7d8d2fe3db58dfa39e21
diablouma <mh732@hotmail.com>
2020-12-13 09:52:42 +0100

    implemented the Option datastructure and up to exercise 4_2

--- src/main/scala/dataStructures/Tree.scala
+++ src/main/scala/dataStructures/Tree.scala
@@ -1,12 +1,12 @@
-package dataStructures
-
-sealed trait Tree[+A]
-case class Leaf[A](value: A) extends Tree[A]
-case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
-
-object Tree {
-  def size[A](tree: Tree[A]): Int = {
-
-  }
-}
-
+//package dataStructures
+//
+//sealed trait Tree[+A]
+//case class Leaf[A](value: A) extends Tree[A]
+//case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
+//
+//object Tree {
+//  def size[A](tree: Tree[A]): Int = {
+//
+//  }
+//}
+//
